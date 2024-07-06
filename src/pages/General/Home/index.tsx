fe7b842650e2c62950b2a97f88de9539042714ae
Sofyan Pujas <gaero38@gmail.com>
2024-07-06 20:26:48 +0800

    add icon

--- src/pages/General/Home/index.tsx
+++ src/pages/General/Home/index.tsx
@@ -1,4 +1,4 @@
-import viteLogo from '/vite.svg'
+import steroidVector from 'assets/steroid.svg'
 import { Anchor, Box, Group, Image, Stack, Text } from '@mantine/core'
 
 const GeneralHomePage = () => {
@@ -7,23 +7,26 @@ const GeneralHomePage = () => {
             <Group className="h-full justify-center">
                 <Stack className="gap-3">
                     <Image
-                        src={viteLogo}
+                        src={steroidVector}
                         alt="Vite Logo"
-                        className="mx-auto size-20 mb-3"
+                        className="mx-auto size-64 mb-3 animate-pulse"
                     />
                     <Stack className="gap-2 text-center">
-                        <Text className="font-bold text-xl">
+                        <Text className="font-bold text-xl text-orange-800">
                             React + Vite + Tailwind CSS + Mantine
                         </Text>
-                        <Text>
-                            {'by '}
-                            <Anchor
-                                href="https://www.github.com/sofyanox12"
-                                target="_blank"
-                            >
-                                @sofyanox12
-                            </Anchor>
-                        </Text>
+                        <Group className='w-full justify-center gap-3'>
+                            <Text>
+                                {'by '}
+                                <Anchor
+                                    href="https://www.github.com/sofyanox12"
+                                    target="_blank"
+                                >
+                                    @sofyanox12
+                                </Anchor>
+                            </Text>
+                            <Box className='animate-spin text-orange-800'>I</Box>
+                        </Group>
                     </Stack>
                 </Stack>
             </Group>
